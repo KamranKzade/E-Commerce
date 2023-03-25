18070756e42e1e70c6172866d6419e531b61b7c2
Kamran <311.kamran@gmail.com>
2023-03-25 08:32:21 +0400

    App.Business Generate

--- App.Business/Concrete/ProductService.cs
+++ App.Business/Concrete/ProductService.cs
@@ -2,11 +2,7 @@
 using App.DataAccess.Abstract;
 using App.DataAccess.Concrete.EfEntityFramework;
 using App.Entities.Concrete;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace App.Business.Concrete
 {
