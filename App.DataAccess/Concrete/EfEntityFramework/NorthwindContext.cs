57314369dc998a89aadd84f6d8918e3e996fe504
Kamran <311.kamran@gmail.com>
2023-03-25 10:24:47 +0400

    Done

--- App.DataAccess/Concrete/EfEntityFramework/NorthwindContext.cs
+++ App.DataAccess/Concrete/EfEntityFramework/NorthwindContext.cs
@@ -8,6 +8,8 @@ namespace App.DataAccess.Concrete.EfEntityFramework
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
         public NorthwindContext()
             :base("Data Source=DESKTOP-E15UN3T;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
         {
