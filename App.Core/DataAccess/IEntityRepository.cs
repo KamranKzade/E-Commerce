df9935a1427489c123323f5c9373652b25f544c4
Kamran <311.kamran@gmail.com>
2023-03-25 08:31:53 +0400

    App.Core Generate

--- App.Core/DataAccess/IEntityRepository.cs
+++ App.Core/DataAccess/IEntityRepository.cs
@@ -1,10 +1,7 @@
 ﻿using App.Core.Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace App.Core.DataAccess
 {
--- App.Core/Entities/IEntity.cs
+++ App.Core/Entities/IEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace App.Core.Entities
+﻿namespace App.Core.Entities
 {
     public interface IEntity
     {
