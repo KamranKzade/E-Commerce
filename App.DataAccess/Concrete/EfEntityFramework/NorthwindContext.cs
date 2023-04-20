1e64d0f1346cc24f85900c2b9b52d2554f85e919
Kamran <311.kamran@gmail.com>
2023-04-20 22:38:02 +0400

    bomba 2

--- App.DataAccess/Concrete/EfEntityFramework/NorthwindContext.cs
+++ App.DataAccess/Concrete/EfEntityFramework/NorthwindContext.cs
@@ -4,14 +4,14 @@ using App.Entities.Concrete;
 
 namespace App.DataAccess.Concrete.EfEntityFramework
 {
-    public class NorthwindContext:DbContext
+    public class NorthwindContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Order> Orders { get; set; }
 
         public NorthwindContext()
-            :base("Data Source=DESKTOP-E15UN3T;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+            : base("Data Source=DESKTOP-E15UN3T;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
         {
 
         }
